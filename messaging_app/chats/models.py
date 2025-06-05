8d150f5fbc80c53576eee6a22ec1ff8e2c445cab
torddey <ea.torddey@gmail.com>
2025-06-05 14:00:21 +0000

    update user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
     
     def __str__(self):
         return self.username

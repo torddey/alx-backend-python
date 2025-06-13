ad876f701b74ae54db9e45b63b2df5f17e4f6513
torddey <ea.torddey@gmail.com>
2025-06-13 15:52:09 +0000

    added logging

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,27 @@
+import logging
+from datetime import datetime
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(message)s',
+    handlers=[
+        logging.FileHandler('requests.Log'),
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+
+    def __call__(self, request):
+        user = request.user.username if hasattr(request, 'user') and request.user.is_authenticated else "Anonymous"
+
+        logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
+
+        response = self.get_response(request)
+        return response
+    
